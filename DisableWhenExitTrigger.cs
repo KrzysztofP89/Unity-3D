c2e672e21ec5d07458c7518ba089caf12bda8197
Krzysiek <k.pacholski89@gmail.com>
2017-12-05 19:40:25 +0100

    optimize_cookbook

---
+++ DisableWhenExitTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DisableWhenExitTrigger : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider hitObjectCollider)
+    {
+        if (hitObjectCollider.CompareTag("Player"))
+        {
+            print("cube close to Player again");
+            enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider hitObjectCollider)
+    {
+        if (hitObjectCollider.CompareTag("Player"))
+        {
+            print("cube away from Player");
+            enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        float d = Vector3.Distance(transform.position, player.transform.position);
+        print(Time.time + ": distance from player to cube = " + d);
+    }
+}
---
+++ DisableWhenNotVisible.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class DisableWhenNotVisible : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnBecameVisible()
+    {
+        enabled = true;
+        print("cube became visible again");
+    }
+
+    void OnBecameInvisible()
+    {
+        enabled = false;
+        print("cube became invisible");
+    }
+
+    void Update()
+    {
+        float d = Vector3.Distance(transform.position, player.transform.position);
+        print(Time.time + ": distance from player to cube = " + d);
+    }
+}
